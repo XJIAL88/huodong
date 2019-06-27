3f1fa56efa7e9609f3dde8b0a245fcfbf3333966
smallPerson <896482826@qq.com>
2019-06-28 01:25:19 +0800

    update

--- src/api/index.js
+++ src/api/index.js
@@ -1,12 +1,12 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "https://etivities-czytest.colourlife.com";
+const baseURL = "https://etivities-czytest.colourlife.com";
 axios.interceptors.response.use(result => result.data);
 
 //=>查看列表
 export let getList = (obj) => {
   let {now_page, page_size} = obj;
-  return axios.get('/backend/activity/list', {
+  return axios.get(`${baseURL}/backend/activity/list`, {
       params: {
         adminId: '3',
         adminToken: 'bc4f4018a91aaaad7eb78327ee6d2949',
@@ -19,7 +19,7 @@ export let getList = (obj) => {
 
 //=>获取资源类型列表
 export let getCategory = () => {
-  return axios.get('/backend/category/list/get', {
+  return axios.get(`${baseURL}/backend/category/list/get`, {
     params: {
       adminId: '3',
       adminToken: 'bc4f4018a91aaaad7eb78327ee6d2949'
@@ -29,7 +29,7 @@ export let getCategory = () => {
 
 //=>获取资源列表
 export let getResource = (num) => {
-  return axios.get('/backend/resource/list/get', {
+  return axios.get(`${baseURL}/backend/resource/list/get`, {
     params: {
       adminId: '3',
       adminToken: 'bc4f4018a91aaaad7eb78327ee6d2949',
@@ -40,7 +40,7 @@ export let getResource = (num) => {
 
 //=>新建活动
 export let create = (a, b, c, d, e) => {
-  return axios.post('/backend/activity/create', {
+  return axios.post(`${baseURL}/backend/activity/create`, {
     adminId: '3',
     adminToken: 'bc4f4018a91aaaad7eb78327ee6d2949',
     name: a,
@@ -54,7 +54,7 @@ export let create = (a, b, c, d, e) => {
 
 //=>查看活动详情
 export let detail = (num) => {
-  return axios.get('/backend/activity/detail', {
+  return axios.get(`${baseURL}/backend/activity/detail`, {
     params: {
       adminId: '3',
       adminToken: 'bc4f4018a91aaaad7eb78327ee6d2949',
@@ -66,7 +66,7 @@ export let detail = (num) => {
 
 //=>活动上下架
 export let update = (id, num) => {
-  return axios.post('/backend/activity/status/update', {
+  return axios.post(`${baseURL}/backend/activity/status/update`, {
     adminId: '3',
     adminToken: 'bc4f4018a91aaaad7eb78327ee6d2949',
     id: id,
@@ -77,7 +77,7 @@ export let update = (id, num) => {
 
 //=>获取活动资源列表
 export let activity = (a, b) => {
-  return axios.get('/backend/activity/detail', {
+  return axios.get(`${baseURL}/backend/activity/detail`, {
     params: {
       adminId: '3',
       adminToken: 'bc4f4018a91aaaad7eb78327ee6d2949',
@@ -92,7 +92,7 @@ export let activity = (a, b) => {
 //=>获取活动资源列表
 export let awardset = (obj) => {
   let {now_page, page_size} = obj;
-  return axios.get('/backend/resource/activity/list', {
+  return axios.get(`${baseURL}/backend/resource/activity/list`, {
     params: {
       adminId: '3',
       adminToken: 'bc4f4018a91aaaad7eb78327ee6d2949',
@@ -106,7 +106,7 @@ export let awardset = (obj) => {
 export let awardsetPrize = (obj) => {
   let {number} = obj;
   console.log(number)
-  return axios.get('/backend/resource/activity/list', {
+  return axios.get(`${baseURL}/backend/resource/activity/list`, {
     params: {
       adminId: '3',
       adminToken: 'bc4f4018a91aaaad7eb78327ee6d2949',
@@ -119,7 +119,7 @@ export let awardsetPrize = (obj) => {
 
 //=>新增奖品
 export let prize = () => {
-  return axios.get('/backend/all/activity/list', {
+  return axios.get(`${baseURL}/backend/all/activity/list`, {
     params: {
       adminId: '3',
       adminToken: 'bc4f4018a91aaaad7eb78327ee6d2949',
@@ -131,7 +131,7 @@ export let prize = () => {
 //=>配置活动奖品池
 export let toConfigure = (obj) => {
   let {activity_id, activity_name, name, resource_id, resource_name, category_id, category_name, resource_number, award_number} = obj;
-  return axios.post('/backend/activity/award/set', {
+  return axios.post(`${baseURL}/backend/activity/award/set`, {
     adminId: '3',
     adminToken: 'bc4f4018a91aaaad7eb78327ee6d2949',
     activity_id: activity_id,
